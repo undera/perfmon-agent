efd5086b6507ba8e6e394b343ae9d608a6c262a2
Juho Saarinen <juho.saarinen@gmail.com>
2013-09-25 08:02:36 +0300

    Back to Java 1.4
    - Java.version back to 1.4
    - Removed powermock
    - Added jUnit back to pom
    - New resource directories to include ServiceLoader -files (src and
    test)
    - Cleaned mocks out of tests

--- test/kg/apc/perfmon/metrics/AbstractPerfMonMetricTest.java
+++ test/kg/apc/perfmon/metrics/AbstractPerfMonMetricTest.java
@@ -1,7 +1,5 @@
 package kg.apc.perfmon.metrics;
 
-import static org.powermock.api.mockito.PowerMockito.when;
-
 import java.util.ServiceLoader;
 
 import junit.framework.TestCase;
@@ -11,19 +9,11 @@ import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
 import org.hyperic.sigar.SigarProxy;
 import org.hyperic.sigar.SigarProxyCache;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.internal.matchers.Any;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  *
  * @author undera
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PerfMonMetricsService.class)
 public class AbstractPerfMonMetricTest extends TestCase {
 
     public AbstractPerfMonMetricTest() {
@@ -54,14 +44,11 @@ public class AbstractPerfMonMetricTest extends TestCase {
      * Test of createMetric method, of class AbstractPerfMonMetric. 
      */
     public void testCreateMetric() {
-		//PerfMonMetricsService mockService = PowerMockito.mock(PerfMonMetricsService.class);
-		//PowerMockito.when(mockService.getMetric(Mockito.anyString(), Mockito.any(MetricParamsSigar.class), Mockito.any(SigarProxy.class))).thenReturn(null);
         System.out.println("createMetric");
         String metricType = "cpu";
         String metricParams = "idle";
         SigarProxy sigarProxy = SigarProxyCache.newInstance(new Sigar(), 500);
         Class expResult = CPUTotalMetric.class;
-        PowerMockito.stub(PowerMockito.method(PerfMonMetricsService.class, "getMetric")).toReturn(new CPUTotalMetric(sigarProxy, new MetricParamsSigar(sigarProxy)));
         AbstractPerfMonMetric result = AbstractPerfMonMetric.createMetric(metricType, metricParams, sigarProxy);
         assertNotNull(result);
         assertEquals(expResult, result.getClass());
--- test/kg/apc/perfmon/metrics/PerfMonMetricsServiceTest.java
+++ test/kg/apc/perfmon/metrics/PerfMonMetricsServiceTest.java
@@ -6,21 +6,9 @@ import java.util.ServiceLoader;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarProxy;
 import org.hyperic.sigar.SigarProxyCache;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.internal.util.reflection.Whitebox;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import junit.framework.TestCase;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.when;
 
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PerfMonMetricsService.class)
 public class PerfMonMetricsServiceTest extends TestCase {
 	
 	PerfMonMetricsService service = PerfMonMetricsService.getInstance();
@@ -31,13 +19,7 @@ public class PerfMonMetricsServiceTest extends TestCase {
 		assertEquals(service, instance);
 	}
 
-	@Test
 	public final void testGetMetric() {
-		Iterator mockIterator = mock(Iterator.class);
-		PowerMockito.stub(PowerMockito.method(ServiceLoader.class, "iterator")).toReturn(mockIterator);
-		when(mockIterator.hasNext()).thenReturn(true);
-		when(mockIterator.next()).thenReturn(new PerfMonMetricsCreatorImpl());
-		
 		String metricType = "cpu";
 		SigarProxy sigarProxy = SigarProxyCache.newInstance(new Sigar(), 500);
 		MetricParamsSigar metricParams = MetricParamsSigar.createFromString("idle", sigarProxy);
